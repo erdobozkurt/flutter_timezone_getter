bda9878a01cf2dbfa84095215ab58d338c445da5
Erdoğan Bozkurt <bozkurt.erdogan@outlook.com>
2024-08-01 01:09:05 +0300

    chore: update repository URL in pubspec.yaml

--- example/timezone_getter_example.dart
+++ example/timezone_getter_example.dart
@@ -2,17 +2,17 @@ import 'package:timezone_getter/timezone_getter.dart';
 
 void main() async {
   // Load timezones
-  final TimezoneGetter timezoneGetter = TimezoneGetter();
-  await timezoneGetter.loadTimezones();
+  final TimezoneGetter tz = TimezoneGetter();
+  await tz.loadTimezones();
 
   // Get timezone abbreviation for example: '+03'
   final String timezoneCode = DateTime.now().timeZoneName;
 
   // Get timezone values
-  final String timezoneName = timezoneGetter.getTimezoneName(timezoneCode);
-  final String timezoneAbbr = timezoneGetter.getTimezoneAbbr(timezoneCode);
-  final double timezoneOffset = timezoneGetter.getTimezoneOffset(timezoneCode);
-  final List<String> timezoneUtc = timezoneGetter.getTimezoneUtc(timezoneCode);
+  final String timezoneName = tz.getTimezoneName(timezoneCode);
+  final String timezoneAbbr = tz.getTimezoneAbbr(timezoneCode);
+  final double timezoneOffset = tz.getTimezoneOffset(timezoneCode);
+  final List<String> timezoneUtc = tz.getTimezoneUtc(timezoneCode);
 
   // Print timezone values
   print('Timezone name: $timezoneName');
